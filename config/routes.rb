e35a0970845a91493b2e3db137066fe312d0b521
Yuval <yvw.bor@gmail.com>
2014-05-22 00:06:15 +0300

    renamed static_page to pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Notpinterest::Application.routes.draw do
 
+
   # You can have the root of your site routed with "root"
-  root :to => 'static_pages#home'
-
-  get "about" => "static_pages#about"
-
+  root :to => 'pages#home'
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
