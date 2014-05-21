af0cfe67cb124e2860665f4cad7909231b26beee
Yuval <yvw.bor@gmail.com>
2014-05-21 23:26:29 +0300

    wish me luck

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Notpinterest::Application.config.secret_key_base = '37bae68bebc87b714c79a6bfe69d3bac80b4297d1dfb8d76a2436341f37d46c95dd39a85ce99ef003f4ad39de017f7d67ecbbebe52b982ee82b330b2a7cb693e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Notpinterest::Application.config.session_store :cookie_store, key: '_notpinterest_session'
